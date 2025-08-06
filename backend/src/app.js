77a9f5bed47377cfe5933f5aa2a80c481bcfcc7f
Tallal64 <firefoxtallal@gmail.com>
2025-08-06 09:20:11 +0500

    production changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,20 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 const app = express();
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // accept data from user/frontend
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cookieParser());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, // allow cookies to be sent
+};
+app.use(cors(corsOptions));
 
 export default app;
 
@@ -19,3 +26,8 @@ import cartRoutes from "./routes/cart.route.js";
 app.use("/api/products", productRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/cart", cartRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (_, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
